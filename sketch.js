450a1c046e1ff1f9a76957a6d7f3915b9e8a0bd7
ganghaaa <119026060+ganghaaa@users.noreply.github.com>
2022-12-09 16:30:39 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,79 @@
-var song;
-var button;
+let musicFile;
+let musicFile2;
+let button;
+let button2;
+let button3;
+let button4
+var jumpButton;
 var t=0;
-var amp;
-//배경
-function setup(){
-    createCanvas(640,480);
-    song = loadSound('ANL.mp3', loaded);
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-    background(51);
+let amp;
+var rB;
+let music;
+
+function setup()
+{
+    createCanvas(640, 480);
+    soundFormats('mp3', 'ogg');
+    musicFile = loadSound('music', loadMusic);
+    musicFile2 = loadSound('music2', loadMusic);
+
+    button = createButton("play");
+    button.mousePressed(togglePlaying1);
+
+    button2 = createButton("Music1");
+    button2.mousePressed(togglePlaying2);
+    music = 1;
+
     amp = new p5.Amplitude();
 }
-
-//플레이하는거
-function togglePlaying(){
-    if (!song.isPlaying()){
-        song.play();
-        song.jump(80);
-        button.html('pause');
-    } else {
-        song.stop();
-        button.html('play');
+function togglePlaying1(){
+    if(music === 1){
+        if(!musicFile.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            musicFile.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile.play();
+            button.html('pause');
+        }else{
+            musicFile.stop();
+            button.html('play');
+        }
     }
-}
-function loaded(){
-    console.log('loaded');
+    if(music === 2){
+        if(!musicFile2.isPlaying()){
+            // rB=musicFile.reverseBuffer();
+            musicFile2.jump(200);
+            musicFile2.setVolume(0.5);
+            musicFile.rate(1);
+            musicFile2.play();
+            button.html('pause');
+        }else{
+            musicFile2.stop();
+            button.html('play');
+        }
+    }
+ }
+
+ function togglePlaying2(){
+    if(music === 1){
+        music = 2;
+        button2.html('Music2');
+    }else{
+        music = 1;
+        button2.html('Music1');
+    }
+    console.log(music);
+ }
+
+function loadMusic()
+{
+    console.log("It's Working");
 }
 
-//누르면 시간이 넘어감
-function mousePressed(){
-    t = t+10;
-    song.jump(t);
-}
-
-//배경변하기
 function draw(){
-    console.log(amp.getLevel());
-    background(255,255,255);
-    noStroke();
-    fill(50,100,00);
-    ellipse(300, 300,amp.getLevel()*500, amp.getLevel()*700)
+console.log(amp.getLevel(), musicFile.duration());
+
+fill(255, 0, 0);
+ellipse(musicFile.currentTime()*20,480-amp.getLevel()*1000, 20, 20);
 
 }
-
